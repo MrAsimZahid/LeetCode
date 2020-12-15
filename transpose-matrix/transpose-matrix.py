cdfe4dd9adaaf361f2cbfd0b59e32e63a0699218
Asim Zahid <asimzahid02@gmail.com>
2020-12-15 22:30:08 +0500

    Time: 132 ms (5.31%), Space: 31.6 MB (6.63%) - LeetHub

---
+++ transpose-matrix/transpose-matrix.py
@@ -0,0 +1,6 @@
+#https://leetcode.com/problems/transpose-matrix/
+#https://github.com/MrAsimZahid
+import numpy
+class Solution:
+    def transpose(self, A: List[List[int]]) -> List[List[int]]:
+        return numpy.transpose(A)
