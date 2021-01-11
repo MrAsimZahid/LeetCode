75644bf6939b6178cba54626fa94e50902879506
Asim Zahid <asimzahid02@gmail.com>
2021-01-11 05:06:14 -0800

    Time: 80 ms (5.57%), Space: 14.1 MB (82.23%) - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/merge-sorted-array/
+#https://github.com/MrAsimZahid
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2[:n])
