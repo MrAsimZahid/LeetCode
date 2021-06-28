b31b925354fc94445e0ad919b449eadb2d41848f
MrAsimZahid <asimzahid02@gmail.com>
2021-06-28 14:14:07 -0700

    complexity added
    O(N^2)

--- 3Sum/3Sum.py
+++ 3Sum/3Sum.py
@@ -5,9 +5,11 @@ class Solution:
     note equation:
     partial_sum = sum - value
     Now send partial_sum into twoSum algo.
+
+    Complexity:  O(N^2)
     """
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        dictionary = {}
+        hashmap = {}
         for outer_index in range(0, len(nums)):
             partial = sum - nums[outer_index]
             for inner_index in range(outer_index, len(nums)):
