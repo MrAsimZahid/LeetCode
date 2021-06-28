7e9934daedd3b222a9dca50d9f3e3256761bfd81
MrAsimZahid <asimzahid02@gmail.com>
2021-06-28 14:04:11 -0700

    3Sum solution manual
    Triple integer TwoSum

---
+++ 3Sum/3Sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    Approch:
+    Hold one index and apply twoSum method.
+    note equation:
+    partial_sum = sum - value
+    Now send partial_sum into twoSum algo.
+    """
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        dictionary = {}
+        for outer_index in range(0, len(nums)):
+            partial = sum - nums[outer_index]
+            for inner_index in range(outer_index, len(nums)):
+                difference = partial - nums[inner_index]
+                if difference in hashmap:
+                    # return all three indices 
+                    return [nums[outer_index], hashmap[difference], inner_index]
+                hashmap[nums[inner_index]] = inner_index
