30aacac9101a677c0564d14c93df0301edf552fe
Asim Zahid <asimzahid02@gmail.com>
2022-02-07 11:05:18 +0500

    Time: 45 ms (49.21%), Space: 13.9 MB (96.95%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,14 +1,23 @@
 # mrasimzahid.github.io
 
-# Time: O(n)
-# Space: O(1)
-
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        total = 0
-        for char in t:
-            total += ord(char)
-        for char in s:
-            total -= ord(char)
-        return chr(total)
+        # solution 1
+        # Time: O(n)
+        # Space: O(n)
+        s_counter = Counter(s)
+        t_counter = Counter(t)
+        for i in set(t):
+            if s_counter[i] < t_counter[i]:
+                return i
+
+        # Time: O(n)
+        # Space: O(1)
+#         # solution 2
+#         total = 0
+#         for char in t:
+#             total += ord(char)
+#         for char in s:
+#             total -= ord(char)
+#         return chr(total)
         
