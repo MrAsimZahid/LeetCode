fc5b06e2ef2ac657a475971c3b61a3816deb6cd9
Asim Zahid <asimzahid02@gmail.com>
2022-03-28 19:12:14 +0500

    Time: 67 ms (63.96%), Space: 14.4 MB (63.51%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,8 @@
+# mrasimzahid.github.io
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if target in nums:
+            return True
+        else:
+            return False
