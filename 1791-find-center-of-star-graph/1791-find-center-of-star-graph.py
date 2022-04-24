effd252abca135a27790d3d4e09938d06f3c1aeb
Asim Zahid <asimzahid02@gmail.com>
2022-04-24 20:21:51 +0500

    Time: 1186 ms (30.18%), Space: 49.9 MB (66.32%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -2,6 +2,13 @@
 
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        edge = edges[0] + edges[1]
-        res = collections.Counter(edge)
-        return res.most_common()[0][0]
+        # Solution 1
+        # edge = edges[0] + edges[1]
+        # res = collections.Counter(edge)
+        # return res.most_common()[0][0]
+        
+        # Solution 2
+        if edges[0][0] in edges[1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]
