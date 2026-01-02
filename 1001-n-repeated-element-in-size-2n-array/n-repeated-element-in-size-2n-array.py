763252b1e49ac0484e75993b51ee989362b6d761
Asim Zahid <asimzahid02@gmail.com>
2026-01-02 11:06:54 +0500

    Time: 17 ms (15.26%) | Memory: 18.7 MB (41.46%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -2,5 +2,12 @@ from collections import Counter
 
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        count = Counter(nums).most_common(1)
-        return count[0][0]
+        nums_half_len = floor(len(nums)/2) 
+        nums_map = {}
+        for i in range(len(nums)):
+            if nums[i] in nums_map:
+                nums_map[nums[i]] = nums_map[nums[i]] + 1
+                if nums_map[nums[i]] >= nums_half_len:
+                    return nums[i]
+            else:
+                nums_map[nums[i]] = 1
